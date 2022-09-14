61d882b95b2e9539a2dfa2154ce8984329322ca1
Patrik Juvonen <patrik.juvonen@evermade.fi>
2022-09-14 12:24:13 +0000

    Update build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'bb9fc1725d540b593b76');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd07853c25a0e74adc4ba');
